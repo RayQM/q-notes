b4319eaa3a3255889e9719d6b6816baef6602793
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2023-12-28 00:39:14 -0700

    add ui

--- app/component/notesViewer.js
+++ app/component/notesViewer.js
@@ -7,14 +7,16 @@ import { Button } from 'react-bootstrap';
 const NotesViewer = ({notes,handleSelectedNote}) =>{
     return(
         <div  style={{ minHeight: "54.5dvh", maxHeight: "54.5dvh"}}  
-        className="bg-secondary flex flex-row p-5 justify-center overflow-auto ">
+        className="bg-secondary flex flex-row p-4 justify-center overflow-auto ">
             <div >
-                <ul className='flex gap-5 flex-wrap'>{notes.map((notes)=>(
-                    <li key={notes.id} className='border h-40 w-32' onClick={() => handleSelectedNote(notes)}>
-                        <div >
-                            <div className='text-black text-center'>{notes.title}</div>
-                            <div style={{minHeight:"10vh", maxWidth:"14vw", wordBreak: "break-word"}} className="flex flex-wrap text-left">{notes.content}</div>
-                            <div>{formatDate(notes)}</div>
+                <ul className='flex gap-6 flex-wrap'>{notes.map((notes)=>(
+                    <li key={notes.id} 
+                    className='rounded-4 w-40 bg-dark text-white ' 
+                    onClick={() => handleSelectedNote(notes)}>
+                        <div className='hover:bg-sky-700 rounded-4'>
+                            <div className='text-center'>{notes.title}</div>
+                            <div style={{minHeight:"18dvh", wordBreak: "break-word"}} className="flex flex-wrap text-left p-3">{notes.content}</div>
+                            <div className='text-center'>{formatDate(notes)}</div>
                         </div>
                     </li>
                 ))}
